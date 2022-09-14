dd1a6371610f1456e3a2e57b3cc76c691ed6db87
Muhammed Ridvan Gider <mridvang@gmail.com>
2022-09-14 11:22:24 +0300

    changed etl.py for file processing and simplified the tranformation

---
+++ etl.py
@@ -0,0 +1,51 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import Column
+from pyspark.sql.functions import upper,split,trim,ascii,substring,substring_index
+from pyspark.sql.types import DecimalType,IntegerType
+import sys
+import boto3
+
+spark = SparkSession.builder.appName("Basit ETL").getOrCreate()
+degree='°'
+
+if len(sys.argv) != 4:
+    print('Usage: etl.py temps cities countries')
+    exit(1)
+
+f_temps = sys.argv[1]
+f_cities = sys.argv[2]
+f_countries = sys.argv[3]
+
+
+df_temps = spark.read \
+        .option("header","true") \
+        .csv(f_temps)
+
+df_cities = spark.read \
+        .option("header","true") \
+        .csv(f_cities)
+
+df_countries = spark.read \
+        .option("header","true") \
+        .csv(f_countries)
+
+df_temps_clean = df_temps.select(
+        df_temps.city,
+        substring_index(split(df_temps.currtemp,'/').getItem(0),degree,1)\
+                .cast(DecimalType(4,1))
+                .alias('temp_c'),
+        substring_index(split(df_temps.currtemp,'/').getItem(1),degree,1)\
+                .cast(DecimalType(4,1))\
+                .alias('temp_f'),
+        substring_index(df_temps.windspeed,' mph',1)\
+                .cast('integer')\
+                .alias('windspeed_mph'),
+        substring_index(df_temps.humidity,'%',1)\
+                .cast('integer')\
+                .alias('humidity_pct')
+
+df_temps2 = df_temps_clean\
+    .join(df_cities, 'city')\  # df_temps_clean.city == df_cities.city
+    .join(df_countries,'country') # c.country = df_countries.country
+
+df_temps2.show(10)    
