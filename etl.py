5c9e5f7f60d04031eedcd882f9a911f242bbcc0b
Muhammed Ridvan Gider <mridvang@gmail.com>
2022-10-29 16:26:09 +0300

    Removed boto3 and added debug option/messages

--- etl.py
+++ etl.py
@@ -5,28 +5,43 @@ from pyspark.sql import Column
 from pyspark.sql.functions import upper,split,trim,ascii,substring,substring_index
 from pyspark.sql.types import DecimalType,IntegerType
 import sys
-import boto3
 
 def process(temps, cities, countries, output):
+    if debug:
+        print('gst-etl: Inside the process()')
+        print('gst-etl: Creating the SparkSession')
+
     spark = SparkSession.builder.appName("Basit ETL").getOrCreate()
     degree='°'
+
+    if debug:
+        print('gst-etl: Reading temps')
 
     df_temps = spark.read \
             .option("header","true") \
             .csv(temps)
+    if debug:
+        print('gst-etl: Reading cities')
 
     df_cities = spark.read \
             .option("header","true") \
             .csv(cities)
+    if debug:
+        print('gst-etl: Reading countries')
 
     df_countries = spark.read \
             .option("header","true") \
             .csv(countries)
+    if debug:
+        print('gst-etl: Cleaning df_countries')
 
     df_countries = df_countries.select(
         trim(df_countries.Continent).alias('Continent'),
         trim(df_countries.Country).alias('Country')
     )
+
+    if debug:
+        print('gst-etl: Cleaning df_temps')
 
     df_temps_clean = df_temps.select(
             df_temps.City,
@@ -44,13 +59,23 @@ def process(temps, cities, countries, output):
                     .alias('Humidity_pct')
                     )
 
+    if debug:
+        print('gst-etl: Createing df_out')
 
     df_out = df_temps_clean\
         .join(df_cities, ['City'],'leftouter')\
         .join(df_countries,['Country'],'leftouter')\
         .select('Continent','Country','City','Temp_C','Temp_F','WindSpeed_Mph','Humidity_pct')
 
+    if debug:
+        print('gst-etl: Writing df_out')
+
     df_out.write.option('header','true').partitionBy('Continent').mode('overwrite').csv(output)
+
+    if debug:
+        print('gst-etl: Exitting process()')
+
+debug = False
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -58,7 +83,17 @@ if __name__ == "__main__":
     parser.add_argument('--cities', help="Cities file")
     parser.add_argument('--countries', help="Countries file")
     parser.add_argument('--output', help="Output file")
+    parser.add_argument('--debug', help="Enable debug messages", action='store_true')
 
     args = parser.parse_args()
+    
+    if args.debug:
+        debug = True
+
+    if debug:
+        print('gst-etl: Parsed the arguments')
+        print('gst-etl: Calling process()')
     process(args.temps,args.cities,args.countries,args.output)
+
+    print('gst-etl: Exitting __main__')
 
